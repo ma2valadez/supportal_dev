c30db8796444781fd369980ada10ffb0a5d7ad22
Michael Valadez <ma2valadez@gmail.com>
2023-03-17 01:04:57 +0000

    Make a basic User model (including secure passwords)

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ApplicationRecord
+    before_save { email.downcase! }
+    before_save { self.email = email.downcase }
+    validates :name, presence: true, length: { maximum: 50 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[firstup.io\d\-]+(\.[firstup.io\d\-]+)*\.[firstup.io]+\z/i
+    validates :email, presence: true, length: { maximum: 255 },
+                      format: { with: VALID_EMAIL_REGEX },
+                      uniqueness: true
+    has_secure_password
+    # def password_requirements_are_met
+    #     rules = {
+    #       " must contain at least one lowercase letter"  => /[a-z]+/,
+    #       " must contain at least one uppercase letter"  => /[A-Z]+/,
+    #       " must contain at least one digit"             => /\d+/,
+    #       " must contain at least one special character" => /[^A-Za-z0-9]+/
+    #     }
+
+    #VALID_PASS_REGEX = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,}$/
+    validates :password, presence: true, length: { minimum: 8 }
+    #           format: { with: VALID_PASS_REGEX }
+
+end
